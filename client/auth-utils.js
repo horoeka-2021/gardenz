21d7fbed2c5d796d4b702c1082fdcfc2581f6129
Eleanor Woodhouse <eleanor.woodhouse@gmail.com>
2022-02-16 12:22:33 +1300

    show auth0 signup by default if registering

--- client/auth-utils.js
+++ client/auth-utils.js
@@ -38,7 +38,7 @@ export function getRegisterFn (useAuth0) {
   const redirectUri = `${window.location.origin}/profile`
   return () => loginWithRedirect({
     redirectUri,
-    screen_hint: 'signin',
+    screen_hint: 'signup',
     scope: 'role:member'
   })
 }
